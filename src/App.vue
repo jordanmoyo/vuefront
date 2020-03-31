ee679a391f9de3687aefb96dc49c1d3ce8cb5fd6
Jordan MOYO <lordmoyojordan@gmail.com>
2020-04-01 00:26:24 +0100

    trying again

--- src/App.vue
+++ src/App.vue
@@ -30,7 +30,7 @@
 </template>
 
 <script>
-import TutorialDataService from './services/TutorialDataService'
+import TutorialDataService from './services/TutorialDataService';
 
 export default {
     name: 'App',
